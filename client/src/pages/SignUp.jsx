bc6eea729ec844ffc860574b3a92919334e346fc
vishnu130902 <vishnu1309n@gmail.com>
2024-03-13 15:03:02 +0530

    completed sign in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,7 +51,7 @@ const SignUp = () => {
         <input type="text" placeholder='username'  className='border p-3 rounded-lg' id='username'  onChange={handleChange}/>
         <input type="email" placeholder='email'  className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type="password" placeholder='password'  className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>{loading ? 'Loading....': 'SignUp'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>{loading ? 'Loading....': 'Sign Up'}</button>
       </form>
       <div className='flex gap-2 mt-5 '>
         <p>Have an account?</p>
